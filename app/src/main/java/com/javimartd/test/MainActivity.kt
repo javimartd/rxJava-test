55ce8adaddcb54b65e591a4789283344fbc15bb5
javimartd <javier.martinez@glownet.com>
2019-12-01 23:46:57 +0100

    feat: included interval operator

--- app/src/main/java/com/javimartd/test/MainActivity.kt
+++ app/src/main/java/com/javimartd/test/MainActivity.kt
@@ -2,12 +2,15 @@ package com.javimartd.test
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import com.javimartd.test.model.CombinePeople
 import com.javimartd.test.model.People
 import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -26,11 +29,43 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 
+/**
+ * The building blocks of RxJava are:
+ *
+ * - Observable: class that emits a stream of data or events. i.e. a class that can be used to perform
+ * some action, and publish the result.
+ *
+ * - Observer: class that receivers the events or data and acts upon it. i.e. a class that waits
+ * and watches the Observable, and reacts whenever the Observable publishes results.
+ *
+ * The Observer has 4 interface methods to know the different states of the Observable.
+ *
+ * onSubscribe(): This method is invoked when the Observer is subscribed to the Observable.
+ * onNext(): This method is called when a new item is emitted from the Observable.
+ * onError(): This method is called when an error occurs and the emission of data is not successfully completed.
+ * onComplete(): This method is called when the Observable has successfully completed emitting all items.
+ *
+ * object : Observer<People> {
+    override fun onNext(t: People) {}
+    override fun onSubscribe(d: Disposable) {}
+    override fun onError(e: Throwable) {}
+    override fun onComplete() {}
+    }
+
+ */
 class MainActivity : AppCompatActivity() {
 
-    lateinit var disposable: Disposable
+    companion object {
+        const val HOST = "https://swapi.co"
+    }
+
+    private val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    private val alphabets = listOf("a", "b", "c", "d", "e", "f")
+
+    private lateinit var disposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +73,14 @@ class MainActivity : AppCompatActivity() {
 
         buttonMakeRequest.setOnClickListener {
             textResponse.text = ""
-            makeSimpleRequestUsingRetrofitAndRxJava()
+            makeRequestUsingJustOperator()
         }
-        buttonStartActivity.setOnClickListener { startActivity<SecondActivity>() }
+
+        buttonStartActivity.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View) {
+                startActivity<SecondActivity>()
+            }
+        })
     }
 
     override fun onDestroy() {
@@ -50,6 +90,60 @@ class MainActivity : AppCompatActivity() {
         if (!disposable.isDisposed) {
             disposable.dispose()
         }
+    }
+
+    private fun intervalOperator() {
+        /*
+        Observable.interval() and Observable.timer() — timer() emits just a single item after a delay whereas
+        interval() operator, on the other hand, will emit items spaced out with a given interval.
+         */
+        Observable.interval(1, TimeUnit.SECONDS)
+            .take(10)
+            .subscribe(object: Observer<Long>{
+                override fun onComplete() {
+                    Log.i("interval operator", "onComplete")
+                }
+
+                override fun onSubscribe(d: Disposable) {
+                    Log.i("interval operator", "onSubscribe")
+                }
+
+                override fun onNext(t: Long) {
+                    Log.i("interval operator", t.toString())
+                }
+
+                override fun onError(e: Throwable) {
+                    Log.i("interval operator", e.message)
+                }
+            })
+    }
+
+    private fun createsOperator() {
+        val observable = Observable.create(object : ObservableOnSubscribe<String> {
+            override fun subscribe(emitter: ObservableEmitter<String>) {
+                try {
+                    alphabets.forEach { emitter.onNext(it) }
+                } catch (e: Exception) {
+                    emitter.onError(e)
+                }
+            }
+        })
+
+        observable
+            .subscribe(object : Observer<String> {
+            override fun onNext(s: String) {
+                Log.i("creates operator", s)
+            }
+            override fun onSubscribe(d: Disposable) {
+                Log.i("creates operator", "onSubscribe")
+            }
+            override fun onError(e: Throwable) {
+                Log.i("creates operator", e.message)
+            }
+            override fun onComplete() {
+                Log.i("creates operator", "onComplete")
+            }
+        })
     }
 
     private fun makeRequestUsingDeferOperator() {
@@ -65,7 +159,11 @@ class MainActivity : AppCompatActivity() {
         disposable = getPeopleObservable("1")
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ showResult(it) }, { showError(it) })
+            .subscribe(
+                { showResult(it) },
+                { showError(it) },
+                { showMessage("onCompleted") },
+                { showMessage("onSubscribe")})
     }
 
     private fun makeRequestUsingFromCallableOperator() {
@@ -120,6 +218,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeRequestUsingJustOperator() {
+        /*
+         This operator takes a list of arguments (maximum 10) and converts the items into Observable items. just()
+         makes only 1 emission. For instance, If an array is passed as a parameter to the just() method, the
+         array is emitted as single item instead of individual numbers. Note that if you pass null to just(),
+         it will return an Observable that emits null as an item.
+         */
+
         disposable = Observable.just(1,2,3)
             .map { (it * 2).toString() }
             .subscribe {item ->
@@ -203,23 +308,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getPeople(person: String): People? {
-        val client = OkHttpClient()
-        val request: Request = Request.Builder()
-            .url("https://swapi.co/api/people/$person")
-            .build()
-        val response = client.newCall(request).execute()
-
-        var people: People? = null
-        if (response.isSuccessful) {
-            people = Gson().fromJson<People>(response.body()?.charStream(), People::class.java)
-        }
-        return people
-    }
-
     private fun makeSimpleRequestUsingRetrofitAndRxJava() {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://swapi.co")
+            .baseUrl(HOST)
+                /*
+                When OkHttp client receives a response from the server, it passes the response back to Retrofit.
+                Retrofit then does its magic: it pushes the meaningless response bytes through converters and wraps
+                it into a usable response with meaningful Java objects. This resource-intensive process is still
+                done on a background thread. Finally, when everything is ready Retrofit needs to return the result
+                to the UI thread of your Android app.
+                The action of returning from the background thread, which receives and prepares
+                the result, to the Android UI thread is a call adapter!
+                 */
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
@@ -228,12 +328,31 @@ class MainActivity : AppCompatActivity() {
         disposable = service.getPeopleObservable("1")
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {showMessage("doOnSubscribe")}
+            .doOnComplete { showMessage("doOnCompleted") }
             .subscribe({ showResult(it)}, { showError(it) })
+    }
+
+    private fun makeRequestSequentially() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(HOST)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val service = retrofit.create(ApiService::class.java)
+
+        disposable = service.getPeopleObservable("1")
+            .map { firstResponse -> "first response:" + firstResponse.name }
+            .flatMap { service.getPeopleObservable("2") }
+            .map { result -> "second response: " + result.name }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { showResult(it) }
     }
 
     private fun makeSimpleRequestUsingRetrofit() {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://swapi.co")
+            .baseUrl(HOST)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
@@ -253,8 +372,31 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun getPeople(person: String): People? {
+        val client = OkHttpClient()
+        val request: Request = Request.Builder()
+            .url("https://swapi.co/api/people/$person")
+            .build()
+        val response = client.newCall(request).execute()
+
+        var people: People? = null
+        if (response.isSuccessful) {
+            people = Gson().fromJson<People>(response.body()?.charStream(), People::class.java)
+        }
+        return people
+    }
+
+    // region other methods
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
     private fun showError(it: Throwable?) {
-        Toast.makeText(this, it?.message, Toast.LENGTH_LONG).show()
+        Toast.makeText(this, it?.message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showResult(message: String) {
+        textResponse.text = message
     }
 
     private fun showResult(it: People?) {
@@ -289,4 +431,5 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         Log.i("MainActivity", "onStop")
     }
+    //endregion
 }
